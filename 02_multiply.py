2f0ab0b14cb7a792b0046797c646ceb7a8103b65
dahye <>
2022-08-26 17:10:20 +0900

    multiply

---
+++ 02_multiply.py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/2588
+def multiply(a, b):
+    sum = 0
+    for i in reversed(range(len(b))):
+        print(a * int(b[i]))
+        sum += a * int(b[i]) * (10 ** (len(b) - i - 1))
+    print(sum)
+
+
+a = int(input())
+b = input()
+multiply(a, b)
