21b7f9f41727face505cefd61c15f29a69eb64e7
dahye <>
2022-09-05 15:51:21 +0900

    성격 유형 검사하기

---
+++ programmers/1LV_personality_type_test.py
@@ -0,0 +1,22 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/118666
+# 문제 :
+def solution(survey, choices):
+    answer = ''
+    score = dict(R=0, T=0, C=0, F=0, J=0, M=0, A=0, N=0)
+    for i in range(len(survey)):
+        if choices[i] > 4:
+            score[survey[i][:1]] += 4 - choices[i]
+        elif choices[i] < 4:
+            score[survey[i][1:]] += choices[i] - 4
+
+    print(score)
+
+    answer += 'R' if score['R'] >= score['T'] else 'T'
+    answer += 'C' if score['C'] >= score['F'] else 'F'
+    answer += 'J' if score['J'] >= score['M'] else 'M'
+    answer += 'A' if score['A'] >= score['N'] else 'N'
+
+    print(answer)
+
+
+solution(["AN", "CF", "MJ", "RT", "NA"], [5, 3, 2, 7, 5])
