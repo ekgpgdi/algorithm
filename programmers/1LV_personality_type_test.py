1c187736ef564ba49087e8b3a7c289a8888e9325
dahye <>
2022-09-05 18:32:26 +0900

    1LV_같은 숫자는 싫어

--- programmers/1LV_personality_type_test.py
+++ programmers/1LV_personality_type_test.py
@@ -8,14 +8,9 @@ def solution(survey, choices):
         elif choices[i] < 4:
             score[survey[i][1:]] += choices[i] - 4
 
-    print(score)
-
     answer += 'R' if score['R'] >= score['T'] else 'T'
     answer += 'C' if score['C'] >= score['F'] else 'F'
     answer += 'J' if score['J'] >= score['M'] else 'M'
     answer += 'A' if score['A'] >= score['N'] else 'N'
 
-    print(answer)
-
-
-solution(["AN", "CF", "MJ", "RT", "NA"], [5, 3, 2, 7, 5])
+    return answer
