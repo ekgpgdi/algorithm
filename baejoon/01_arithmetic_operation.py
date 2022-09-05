490137ae01f19ca4713d102d8c8c479d7aa4a05a
dahye <>
2022-09-05 14:38:52 +0900

    10869 사칙 연산

--- baejoon/01_arithmetic_operation.py
+++ baejoon/01_arithmetic_operation.py
@@ -1,7 +1,7 @@
 # https://www.acmicpc.net/problem/10869
 # 문제 : 두 자연수 a와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오.
 # 입력 : 두 자연수 A와 B가 주어진다. (1 ≤ A, B ≤ 10,000)
-# 출력 : 첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+# 출력 : 첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다. 
 
 # 더하기
 def plus(a, b):
@@ -29,7 +29,7 @@ def remainder(a, b):
 
 
 def arithmetic_operation(a, b):
-    print(plus(a, b))  
+    print(plus(a, b))
     print(minus(a, b))
     print(multiply(a, b))
     print(divide(a, b))
