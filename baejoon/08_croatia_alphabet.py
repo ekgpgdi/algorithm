8f2ca8953f922090626e1432a2f56b675b19993c
dahye <>
2022-09-05 14:42:13 +0900

    2941 크로아티아 알파벳

--- baejoon/08_croatia_alphabet.py
+++ baejoon/08_croatia_alphabet.py
@@ -1,5 +1,5 @@
 # https://www.acmicpc.net/problem/2941
-# 입력으로 주어진 단어가 몇 개의 크로아티아 알파벳으로 이루어져 있는지 출력 
+# 입력으로 주어진 단어가 몇 개의 크로아티아 알파벳으로 이루어져 있는지 출력
 def croatia_alphabet(word):
     change_word = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
     start_word = ["c", "d", "l", "n", "s", "z"]
